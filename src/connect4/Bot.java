3aa35d42763ee6c013fa52a6ac9380645b66efc4
Pirate2737 <64175842+Pirate2737@users.noreply.github.com>
2023-10-31 19:57:43 -0400

    started implementation of doTurn
    eyes

--- src/connect4/Bot.java
+++ src/connect4/Bot.java
@@ -1,13 +1,28 @@
 package connect4;
 
 public class Bot extends Player {
+	
+	final int PIECE = 2; // hopefully temporary
 
 	public Bot(){
 		super("Bot", 2);
 	}
 	
+	public void doTurn(int turns) {
+		if(((int)(Math.random()*1))==1) {
+			randTurn();
+		}else {
+			// turn one claim middle
+			if(turns==1) {
+				Board.setBoard(0, 3, PIECE);
+			}
+			else {randTurn();}
+			
+			
+		}
+	}
+	
 	public void randTurn() {
-
 		String input;
 		
 		do {
@@ -18,11 +33,11 @@ public class Bot extends Player {
 		
 		for(int r=Board.getBoard()[0].length; r>-1; r--) {
 			if(Board.getBoard()[r][col]==0) {
-				Board.setBoard(r, col, 2);
+				Board.setBoard(r, col, PIECE);
 				break;
 			}
 		}
 		
-	}// end of doTurn()
+	}// end of randTurn()
 
 }// end of class
