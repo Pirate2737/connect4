b9c7c666a782f5c0bf10e68bc51979058e796c1e
Pirate2737 <64175842+Pirate2737@users.noreply.github.com>
2023-11-02 09:33:59 -0400

    made the bot not cheat

--- src/connect4/Bot.java
+++ src/connect4/Bot.java
@@ -12,16 +12,19 @@ public class Bot extends Player {
 	public void doTurn(int turns) {
 		sandbox = Board.getBoard();
 		
+		// get win
 		for(int r=sandbox.length-1; r>-1; r--) {
 			for(int c=0; c<sandbox[0].length; c++) {
 				int temp = sandbox[r][c];
-				sandbox[r][c] = PIECE;
-				if(Board.checkOver(sandbox, 4)==PIECE) {
-					Board.setBoard(r, c, PIECE);
-					Board.setOver(false);
-					return;
+				if(temp==0) {
+					sandbox[r][c] = PIECE;
+					if(Board.checkOver(sandbox, 4)==PIECE && super.isValidMove(String.valueOf(c))!=-1) {
+						Board.setBoard(r, c, PIECE);
+						Board.setOver(false);
+						return;
+					}
+					sandbox[r][c] = temp;
 				}
-				sandbox[r][c] = temp;
 			}
 		}
 		
@@ -33,16 +36,19 @@ public class Bot extends Player {
 				Board.setBoard(0, 3, PIECE);
 				return;
 			}
+			// block player win
 			for(int r=sandbox.length-1; r>-1; r--) {
 				for(int c=0; c<sandbox[0].length; c++) {
 					int temp = sandbox[r][c];
-					sandbox[r][c] = PIECE-1;
-					if(Board.checkOver(sandbox, 4)==PIECE-1) {
-						Board.setBoard(r, c, PIECE);
-						Board.setOver(false);
-						return;
+					if(temp==0) {
+						sandbox[r][c] = PIECE-1;
+						if(Board.checkOver(sandbox, 4)==PIECE-1 && super.isValidMove(String.valueOf(c))!=-1) {
+							Board.setBoard(r, c, PIECE);
+							Board.setOver(false);
+							return;
+						}
+						sandbox[r][c] = temp;
 					}
-					sandbox[r][c] = temp;
 				}
 			}
 			
